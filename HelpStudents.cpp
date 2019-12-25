8ce0296e21b78dcc90a31a08441ec41dac826d06
zcanfes <56366573+zcanfes@users.noreply.github.com>
2019-12-25 14:03:54 +0300

    Add files via upload

---
+++ HelpStudents.cpp
@@ -0,0 +1,33 @@
+/*
+Student Name:
+Student Number:
+Project Number: 4
+Compile Status: [SUCCESS/FAIL]
+Running Status: [SUCCESS/FAIL]
+Notes: Anything you want to say about your code that will be helpful in the grading process.
+*/
+#include "HelpStudents.h"
+
+using namespace std;
+
+HelpStudents::HelpStudents(int  N, int  M, int K, vector < pair< pair <int,int> , int > > ways) {
+    // IMPLEMENT ME!
+}
+
+long long int HelpStudents::firstStudent() {
+    // IMPLEMENT ME!
+}
+long long int HelpStudents::secondStudent() {
+    // IMPLEMENT ME!
+}
+long long int HelpStudents::thirdStudent() {
+    // IMPLEMENT ME!
+}
+long long int HelpStudents::fourthStudent() {
+    // IMPLEMENT ME!
+}
+long long int HelpStudents::fifthStudent() {
+    // IMPLEMENT ME!
+}
+
+// YOU CAN ADD YOUR HELPER FUNCTIONS
